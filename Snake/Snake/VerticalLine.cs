f289c599ff594b09a6b2e1b13fa07a5721b098be
Daniel <tercon2011@gmail.com>
2016-04-03 18:53:25 +0400

    8th lesson
    Inheritance

--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HorizontalLine : Figure
-    {
-        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+    class VerticalLine : Figure
+    { 
+
+        public VerticalLine(int yTop, int yBottom, int x, char sym)
         {
             pList = new List<Point>();
-            for (int x = xLeft; x <= xRight; x++)
+            for (int y = yTop; y <= yBottom; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
-            
         }
     }
 }
